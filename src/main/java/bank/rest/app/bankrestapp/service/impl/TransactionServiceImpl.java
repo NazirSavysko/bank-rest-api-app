edceec6460cc73567ed8f0b47d0c4107bd6f8da3
Nazir Savysko <nazirsavisko@gmail.com>
2025-07-26 20:18:20 +0300

    Add email verification check in withdrawal process: inject `EmailService` into `TransactionServiceImpl` and validate sender's email before processing transactions.

--- src/main/java/bank/rest/app/bankrestapp/service/impl/TransactionServiceImpl.java
+++ src/main/java/bank/rest/app/bankrestapp/service/impl/TransactionServiceImpl.java
@@ -2,37 +2,43 @@ package bank.rest.app.bankrestapp.service.impl;
 
 import bank.rest.app.bankrestapp.currency.CurrencyLoader;
 import bank.rest.app.bankrestapp.entity.Account;
-import bank.rest.app.bankrestapp.entity.Card;
+import bank.rest.app.bankrestapp.entity.Customer;
 import bank.rest.app.bankrestapp.entity.enums.Currency;
 import bank.rest.app.bankrestapp.resository.AccountRepository;
-import bank.rest.app.bankrestapp.resository.CardRepository;
+import bank.rest.app.bankrestapp.service.EmailService;
 import bank.rest.app.bankrestapp.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.NoSuchElementException;
 
-import static java.math.RoundingMode.HALF_UP;
 
 @Service
 public class TransactionServiceImpl implements TransactionService {
 
     private final AccountRepository accountRepository;
     private final CurrencyLoader currencyLoader;
+    private final EmailService emailService;
 
     @Autowired
     public TransactionServiceImpl(final AccountRepository accountRepository,
+                                  final EmailService emailService,
                                   final CurrencyLoader currencyLoader) {
         this.accountRepository = accountRepository;
         this.currencyLoader = currencyLoader;
+        this.emailService = emailService;
     }
 
     @Override
     public void withdraw(final String senderCardNumber, final String recipientCardNumber, final BigDecimal amount) {
-       final Account senderAccount = getAccountByCardNumber(senderCardNumber);
+
+        final Account senderAccount = getAccountByCardNumber(senderCardNumber);
         final Account recipientAccount = getAccountByCardNumber(recipientCardNumber);
+
+        final Customer senderCustomer = senderAccount.getCustomer();
+
+        this.emailService.checkIfCodeIsVerified(senderCustomer.getAuthUser().getEmail());
 
         if (senderAccount.getBalance().compareTo(amount) < 0) {
             throw new IllegalArgumentException("Insufficient funds in sender's account");
