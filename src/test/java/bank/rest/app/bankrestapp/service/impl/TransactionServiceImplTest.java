4c09c72b200deeda1c7fdb56bbbc063be2c83a4e
Nazir Savysko <ulabulgakova07@gmail.com>
2026-02-25 17:56:48 +0200

    Simplify Mockito imports in TransactionServiceImplTest

--- src/test/java/bank/rest/app/bankrestapp/service/impl/TransactionServiceImplTest.java
+++ src/test/java/bank/rest/app/bankrestapp/service/impl/TransactionServiceImplTest.java
@@ -24,8 +24,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class TransactionServiceImplTest {
